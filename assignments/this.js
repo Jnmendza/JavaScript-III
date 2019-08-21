2413abea09c70e18dedf6b7c2ae1dfe789b4492c
Jnmendza <jonatmendoza@gmail.com>
2019-08-21 13:53:50 -0700

    MVPs completed for this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,72 @@
-/* The for principles of "this";
+/* The four principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding - "this" is referring to the window if your code isn't pointing at something specific.
+* 2. Implicit Binding - the keyword refers to anything left of the dot (.) and it only applies to objects with methodds
+* 3. Explicit Binding - by using call, apply or bind you can invoke a function for "this"
+* 4. NEW Binding - it is used to creat new objects
 *
 * write out a code example of each explanation above
 */
 
-// Principle 1
-
+// Principle 1 
 // code example for Window Binding
+function tiger(){
+    console.log(this.prey);
+}
+
+tiger();
+const prey = 'deer';
+
+
 
 // Principle 2
-
 // code example for Implicit Binding
 
-// Principle 3
+const person = {
+    name: 'Jim',
+    greeting: 'Whats up my name is ',
+    words: function(){
+        return `${this.greeting} ${this.name}`
+    }
+}
+console.log(person.words());
 
+
+// Principle 3
 // code example for New Binding
 
-// Principle 4
+function Sport(fav){
+    this.game = fav;
+}
 
+let myFavSport = new Sport('Futbol');
+
+console.log (`My favorite sport is ${myFavSport.game}.`);
+
+// Principle 4
 // code example for Explicit Binding
+// .call
+function tiger(){
+    console.log(this.food);
+}
+
+let myTiger = {
+    name: 'Tony',
+    food: 'Korn Flakes'
+}
+
+tiger.call(myTiger);
+
+// .bind
+function tiger(){
+    console.log(this.food);
+}
+
+let myTiger = {
+    name: 'Tony',
+    food: 'Korn Flakes'
+}
+
+tonyTheTiger = tiger.bind(myTiger);
+tonyTheTiger();
